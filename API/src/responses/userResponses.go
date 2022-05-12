24f5b8538b9bacb1f153afc5fa13e447b035628a
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-12 02:17:12 -0300

    setting content type of responses

--- API/src/responses/userResponses.go
+++ API/src/responses/userResponses.go
@@ -8,6 +8,7 @@ import (
 
 // Returns a JSON response to a request
 func JSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
