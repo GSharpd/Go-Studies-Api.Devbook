8845fa7ab234a3a564de59b4d52c4bfbbc674ee3
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-12 00:45:45 -0300

    implement call for dbclose

--- API/src/controllers/usersController.go
+++ API/src/controllers/usersController.go
@@ -28,6 +28,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 
 	repo := repositories.NewUserRepository(db)
 	userID, err := repo.Create(user)
