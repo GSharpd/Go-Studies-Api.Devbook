62fd41ea0d0ba6f3265b5874c2d0f42c4043b261
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-12 00:57:37 -0300

    add userResponses.go

---
+++ API/src/responses/userResponses.go
@@ -0,0 +1,25 @@
+package responses
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Returns a JSON response to a request
+func JSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Returns an error JSON response to a request
+func ErrorResponse(w http.ResponseWriter, statusCode int, err error) {
+	JSONResponse(w, statusCode, struct {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
+}
