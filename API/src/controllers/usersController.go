891d95ec4e075a83577b281bbcee1aeb9a58774d
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-11 23:00:41 -0300

    change file name and call to repo

---
+++ API/src/controllers/usersController.go
@@ -0,0 +1,54 @@
+package controllers
+
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Creates a new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := repositories.NewUserRepository(db)
+	repo.Create(user)
+
+}
+
+// Gets all users from the database
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gettting all users"))
+}
+
+// Gets a specific user from the database
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gettting user"))
+}
+
+// Updates user information in the database
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Updating user"))
+}
+
+// Deletes a user from the database
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deleting user"))
+}
