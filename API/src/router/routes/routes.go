0d846371475000331fb8ca5bb95f86c5ea9c864d
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-14 16:01:37 -0300

    adds login route

--- API/src/router/routes/routes.go
+++ API/src/router/routes/routes.go
@@ -17,6 +17,7 @@ type Route struct {
 // Configure all user routes to the given router
 func ConfigureRoutes(r *mux.Router) *mux.Router {
 	routes := usersRoutes
+	routes = append(routes, loginRoute)
 
 	for _, route := range routes {
 		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
