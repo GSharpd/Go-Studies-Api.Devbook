b2fda7996446cb53ff01e15fa29cc80ea1ab0ad5
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-15 13:56:32 -0300

    adds token validation to midware

--- API/src/middlewares/middlewares.go
+++ API/src/middlewares/middlewares.go
@@ -1,23 +1,28 @@
 package middlewares
 
 import (
-	"fmt"
+	"api/src/authentication"
+	"api/src/responses"
 	"log"
 	"net/http"
 )
 
 // Verifies if client making a request is authenticated
-func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+func Authenticate(nextFunction http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Validating Token...")
-		next(w, r)
+		if err := authentication.ValidateToken(r); err != nil {
+			responses.ErrorResponse(w, http.StatusUnauthorized, err)
+			return
+		}
+
+		nextFunction(w, r)
 	}
 }
 
 //Logger logs informations from requests in the terminal
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(nextFunction http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s, %s, %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		nextFunction(w, r)
 	}
 }
