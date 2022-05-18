1fda7385ac264783f74b4382a712da5e0eaa4738
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-17 21:23:24 -0300

    create posts routes

---
+++ API/src/router/routes/posts.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var postsRoutes = []Route{
+	{
+		URI:                    "/posts",
+		Method:                 http.MethodPost,
+		Function:               controllers.CreatePost,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/posts",
+		Method:                 http.MethodGet,
+		Function:               controllers.GetPosts,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/posts/{id}",
+		Method:                 http.MethodGet,
+		Function:               controllers.GetPostByID,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/posts/{id}",
+		Method:                 http.MethodPut,
+		Function:               controllers.UpdatePost,
+		RequiresAuthentication: true,
+	},
+	{
+		URI:                    "/posts/{id}",
+		Method:                 http.MethodDelete,
+		Function:               controllers.DeletePost,
+		RequiresAuthentication: true,
+	},
+}
